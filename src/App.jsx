a9b91fb81011549ac659276a797353a207bae1d1
Anubhav Mehrotra <anubhavmehrotra04@gmail.com>
2026-07-07 03:35:48 +0530

    Initial commit2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-function apple (){
+function App(){
+
+  // alert(sum())
   return(
-  <h1>Hello</h1>
+    <div>
+    <h1>First Component {sum()}</h1>
+     <Fruit />
+     <Colors />
+    </div>
+  )
+}
+function Fruit(){
+  return(
+    <h1>Apple</h1>
   )
 }
 
-export default apple
+function Colors(){
+  return(
+    <h1>Red Color</h1>
+  )
+}
+
+function sum(){
+  return 10+10
+}
+export default App;
